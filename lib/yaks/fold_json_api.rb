42bf775821686d6249b4c3e6f9f667a077aa1c2f
Arne Brasseur <arne@arnebrasseur.net>
2013-12-18 19:24:34 -0300

    Add preliminary support for URLs in JSON-API

--- lib/yaks/fold_json_api.rb
+++ lib/yaks/fold_json_api.rb
@@ -2,25 +2,31 @@
 
 module Yaks
   class FoldJsonApi
-    include Concord.new(:collection)
+    include Concord.new(:collection, :options)
     include Util
     extend Forwardable
 
     def_delegator :collection, :root_key
 
-    def self.call(collection)
-      new(collection).fold
+    def self.call(collection, options)
+      new(collection, options).fold
     end
 
     def fold
-      if collection.empty?
-        {}
-      else
-        {
-          root_key => collection.map(& μ(:fold_object) ),
-          "linked" => fold_associated_objects
-        }
+      result = Hash(
+        root_key => collection.map(& μ(:fold_object) ),
+        "linked" => fold_associated_objects
+      )
+      if @options[:url_lookup]
+        result = result.put('links',
+          Hash(
+            ([root_key] + association_paths).map do |path|
+              [ path, @options[:url_lookup].(path) ]
+            end
+          )
+        )
       end
+      result
     end
     alias call fold
 
@@ -46,12 +52,21 @@ module Yaks
       end
     end
 
-    def fold_associated_objects
-      association_names = Set(*
+    def association_names
+      Set(*
         collection.flat_map do |object|
           object.associations.map{|ass| [ass.name, ass.one?] }
         end
       )
+    end
+
+    def association_paths
+      association_names.map do |name, one|
+        "#{root_key}.#{name}"
+      end
+    end
+
+    def fold_associated_objects
       Hash(
         association_names.map do |name, one|
           objects = collection.flat_map(& σ(:associated_objects, name) )
@@ -62,5 +77,6 @@ module Yaks
         end
       )
     end
+
   end
 end
