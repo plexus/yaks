52b16caa5efc15fa7159072ea84c3e87a3a55e57
Arne Brasseur <arne@arnebrasseur.net>
2014-05-25 22:20:15 +0800

    Add explicit coverage for CollectionMapper, inherit from Mapper

--- lib/yaks/collection_mapper.rb
+++ lib/yaks/collection_mapper.rb
@@ -1,36 +1,22 @@
 # -*- coding: utf-8 -*-
 
 module Yaks
-  class CollectionMapper
-    include Util, FP
-    extend Mapper::ClassMethods
-
-    attr_reader :collection, :resource_mapper, :policy
-    private :collection, :resource_mapper
-
-    def_delegators 'self.class', :config
-    def_delegators :config, :links
+  class CollectionMapper < Mapper
+    attr_reader :collection, :resource_mapper
+    alias collection subject
 
     def initialize(collection, resource_mapper, policy)
-      @collection      = collection
+      super(collection, policy)
       @resource_mapper = resource_mapper
-      @policy          = policy
     end
 
     def to_resource
       CollectionResource.new(
         type: resource_mapper.config.type || policy.derive_type_from_mapper_class(resource_mapper),
         links: map_links,
+        attributes: map_attributes,
         members: collection.map {|obj| resource_mapper.new(obj, policy).to_resource }
       )
-    end
-
-    def load_attribute(name)
-      respond_to?(name) ? send(name) : collection.map(&name.to_sym)
-    end
-
-    def map_links
-      links.map &send_with_args(:map_to_resource_link, self)
     end
 
   end
---
+++ spec/unit/yaks/collection_mapper_spec.rb
@@ -0,0 +1,78 @@
+require 'spec_helper'
+
+describe Yaks::CollectionMapper do
+  include_context 'fixtures'
+
+  subject(:mapper) { described_class.new(collection, resource_mapper, policy) }
+  let(:collection) { [] }
+  let(:resource_mapper) { Class.new(Yaks::Mapper) { type 'the_type' } }
+  let(:policy) { Yaks::DefaultPolicy.new }
+
+  its(:to_resource) {
+    should eql Yaks::CollectionResource.new(
+        type: 'the_type',
+        links: [],
+        attributes: {},
+        members: []
+    )
+  }
+
+  context 'with members' do
+    let(:collection) { [boingboing, wassup]}
+    let(:resource_mapper) { PetMapper }
+
+    its(:to_resource) {
+      should eql Yaks::CollectionResource.new(
+        type: 'pet',
+        links: [],
+        attributes: {},
+        members: [
+          Yaks::Resource.new(type: 'pet', attributes: {:id => 2, :species => "dog", :name => "boingboing"}),
+          Yaks::Resource.new(type: 'pet', attributes: {:id => 3, :species => "cat", :name => "wassup"})
+        ]
+      )
+    }
+  end
+
+  context 'with collection attributes' do
+    subject(:mapper) {
+      Class.new(Yaks::CollectionMapper) do
+        attributes :foo, :bar
+      end.new(collection, resource_mapper, policy)
+    }
+
+    let(:collection) {
+      Class.new(SimpleDelegator) do
+        def foo ; 123 ; end
+        def bar ; 'pi la~~~' ; end
+      end.new([])
+    }
+
+    its(:to_resource) {
+      should eql Yaks::CollectionResource.new(
+        type: 'the_type',
+        links: [],
+        attributes: { foo: 123, bar: 'pi la~~~' },
+        members: []
+      )
+    }
+  end
+
+  context 'with collection links' do
+    subject(:mapper) {
+      Class.new(Yaks::CollectionMapper) do
+        link :self, 'http://api.example.com/orders'
+      end.new(collection, resource_mapper, policy)
+    }
+
+    its(:to_resource) {
+      should eql Yaks::CollectionResource.new(
+        type: 'the_type',
+        links: [ Yaks::Resource::Link.new(:self, 'http://api.example.com/orders', {}) ],
+        attributes: { },
+        members: []
+      )
+    }
+  end
+
+end
