0bea2fbd398e02109a62bf1d988a7f91a358489a
Arne Brasseur <arne@arnebrasseur.net>
2014-11-25 16:16:06 +0100

    Add some convenience methods to Yaks::Resource::Form and ::Field
    Access form values with [] or get the value hash with #values. Resolve
    the value of a select list correctly.

--- yaks/lib/yaks/resource/form.rb
+++ yaks/lib/yaks/resource/form.rb
@@ -3,8 +3,28 @@ module Yaks
     class Form
       include Yaks::Mapper::Form.attributes
 
+      def [](name)
+        fields.find {|field| field.name == name}.value
+      end
+
+      def values
+        fields.each_with_object({}) do |field, values|
+          values[field.name] = field.value
+        end
+      end
+
       class Field
-        include Yaks::Mapper::Form::Field.attributes
+        include Yaks::Mapper::Form::Field.attributes.add(:error => nil)
+
+        def value(arg = Undefined)
+          return @value if arg.eql?(Undefined)
+          if type == :select
+            selected = options.find { |option| option.selected }
+            selected.value if selected
+          else
+            update(value: arg)
+          end
+        end
       end
     end
   end
