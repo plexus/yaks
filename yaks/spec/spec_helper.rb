bdb3ba2ded89cff677aafb63315daf6e64f631dd
Dane Lowe <dane@avidonline.co.nz>
2015-03-12 13:49:46 +0000

    Remove changes to specs.

--- yaks/spec/spec_helper.rb
+++ yaks/spec/spec_helper.rb
@@ -24,9 +24,9 @@ RSpec.configure do |rspec|
   rspec.backtrace_exclusion_patterns = [] if ENV['FULLSTACK']
   rspec.disable_monkey_patching!
   rspec.raise_errors_for_deprecations!
-  # rspec.around(:each) do |example|
-  #   Timeout.timeout(1, &example)
-  # end
+  rspec.around(:each) do |example|
+    Timeout.timeout(1, &example)
+  end
 end
 
 Bogus.configure do |bogus|
