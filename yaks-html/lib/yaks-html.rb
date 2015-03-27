a7055a230fad03ee07a5bdfee1bdf0ce4f6f895b
Arne Brasseur <arne@arnebrasseur.net>
2015-03-27 16:53:21 +0100

    Stop Hexp from escaping `<style>` stuff
    This will be fixed in Hexp as well, but this way people aren't forced to
    upgrade.

--- yaks-html/lib/yaks-html.rb
+++ yaks-html/lib/yaks-html.rb
@@ -3,6 +3,6 @@ require 'yaks'
 require 'yaks/format/html'
 
 -> do
-  unparser = Hexp::Unparser.new({})
+  unparser = Hexp::Unparser.new({no_escape: [:script, :style]})
   Yaks::Serializer.register(:html, ->(data, env) { unparser.call(data) })
 end.call
