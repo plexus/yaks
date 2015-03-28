c6e4b4afcc73f0b10a702612dd3fea107c8e0c13
Arne Brasseur <arne@arnebrasseur.net>
2015-03-28 11:10:24 +0100

    Bring Yaks::Builder to 100% explicit mutcov

--- yaks/spec/unit/yaks/builder_spec.rb
+++ yaks/spec/unit/yaks/builder_spec.rb
@@ -22,26 +22,50 @@ RSpec.describe Yaks::Builder do
     end
   end
 
-  it 'should keep state' do
-    expect(
-      subject.create(3, 4) do
-        foo 7
-        update bar: 6
-      end.to_h
-    ).to eql(foo: 7, bar: 6)
-  end
-
-  it 'should unwrap again' do
-    expect( subject.create(3, 4) { finalize } ).to eql Buildable.new(foo: 7, bar: 8)
-  end
-
-  context 'with no methods to forward' do
-    subject do
-      Yaks::Builder.new(Buildable)
+  describe '#initialize' do
+    it 'should set the class to be instantiated' do
+      expect(subject.create(1,2)).to be_instance_of Buildable
     end
 
-    it 'should still work' do
-      expect(subject.create(3,4)).to eql Buildable.new(foo: 3, bar: 4)
+    it 'should accept just a class' do
+      expect { Yaks::Builder.new(Buildable) }.to_not raise_error
+    end
+
+    it 'should define delegators for passed in method names' do
+      expect(subject).to respond_to(:finalize)
+    end
+
+    it 'should store the methods for inspection' do
+      expect(subject.inspect).to eql "#<Builder Buildable [:finalize]>"
+    end
+
+    it 'should evaluate the passed in block' do
+      expect(subject).to respond_to(:foo)
+    end
+  end
+
+  describe '#create' do
+    it 'should keep state' do
+      expect(
+        subject.create(3, 4) do
+          foo 7
+          update bar: 6
+        end.to_h
+      ).to eql(foo: 7, bar: 6)
+    end
+
+    it 'should unwrap again' do
+      expect( subject.create(3, 4) { finalize } ).to eql Buildable.new(foo: 7, bar: 8)
+    end
+
+    context 'with no methods to forward' do
+      subject do
+        Yaks::Builder.new(Buildable)
+      end
+
+      it 'should still work' do
+        expect(subject.create(3,4)).to eql Buildable.new(foo: 3, bar: 4)
+      end
     end
   end
 
