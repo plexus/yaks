04249f821a1e2b0b1d53741542f3b453942c80e8
Arne Brasseur <arne@arnebrasseur.net>
2015-03-30 12:46:39 +0200

    Bring Yaks::Resource to 100% explicit mutcov

--- yaks/spec/unit/yaks/resource_spec.rb
+++ yaks/spec/unit/yaks/resource_spec.rb
@@ -2,93 +2,18 @@ RSpec.describe Yaks::Resource do
   subject(:resource) { described_class.new(init_opts) }
   let(:init_opts) { {} }
 
-  context 'with a zero-arg constructor' do
-    subject(:resource) { described_class.new }
-
-    its(:type)           { should be_nil }
-    its(:attributes)     { should eql({}) }
-    its(:links)          { should eql [] }
-    its(:subresources)   { should eql [] }
-    its(:self_link)      { should be_nil }
-    its(:null_resource?) { should be false }
-    its(:collection?)    { should be false }
-  end
-
-  context 'with a type' do
-    let(:init_opts) { { type: 'post' } }
-    its(:type) { should eql 'post' }
-  end
-
-  context 'with attributes' do
-    let(:init_opts) { { attributes: {name: 'Arne', age: 31} } }
-
-    it 'should delegate [] to attribute access' do
-      expect(resource[:name]).to eql 'Arne'
-    end
-  end
-
-  context 'with links' do
-    let(:init_opts) {
-      {
-        links: [
-          Yaks::Resource::Link.new(rel: :profile, uri: '/foo/bar/profile'),
-          Yaks::Resource::Link.new(rel: :self, uri: '/foo/bar')
-        ]
-      }
-    }
-    its(:links) { should eql [
-        Yaks::Resource::Link.new(rel: :profile, uri: '/foo/bar/profile'),
-        Yaks::Resource::Link.new(rel: :self, uri: '/foo/bar')
-      ]
-    }
-
-    its(:self_link) { should eql Yaks::Resource::Link.new(rel: :self, uri: '/foo/bar') }
-  end
-
-  context 'with subresources' do
-    let(:init_opts) { { subresources: [ Yaks::Resource.new(type: 'comment', rels: ['comments']) ] } }
-    its(:subresources) { should eql [Yaks::Resource.new(type: 'comment', rels: ['comments'])]  }
-
-    it 'should return an enumerable for #seq' do
-      expect(resource.seq.each.with_index.to_a).to eq  [ [resource, 0] ]
-    end
-  end
-
-  it 'should act as a collection of one' do
-    expect(resource.seq.each.to_a).to eql [resource]
-  end
-
-  describe 'persistent updates' do
-    let(:resource) {
-      Yaks::Resource.new(
-        attributes: {x: :y},
-        links: [:one],
-        subresources: [ :subres ]
-      )
-    }
-
-    it 'should do updates without modifying the original' do
-      expect(
-        resource
-          .merge_attributes(foo: :bar)
-          .add_link(:a_link)
-          .add_subresource(:a_subresource)
-          .merge_attributes(foo: :baz)
-      ).to eq Yaks::Resource.new(
-        attributes: {x: :y, foo: :baz},
-        links: [:one, :a_link],
-        subresources: [:subres, :a_subresource]
-      )
-
-      expect(resource).to eq Yaks::Resource.new(
-        attributes: {x: :y},
-        links: [:one],
-        subresources: [:subres]
-      )
-    end
-  end
-
   describe '#initialize' do
+    context 'with a zero-arg constructor' do
+      subject(:resource) { described_class.new }
+
+      its(:type)           { should be_nil }
+      its(:attributes)     { should eql({}) }
+      its(:links)          { should eql [] }
+      its(:subresources)   { should eql [] }
+      its(:self_link)      { should be_nil }
+      its(:null_resource?) { should be false }
+    end
+
     it 'should verify subresources is an array' do
       expect { Yaks::Resource.new(subresources: { '/rel/comments' => []}) }
         .to raise_exception /comments/
@@ -108,6 +33,28 @@ RSpec.describe Yaks::Resource do
     end
   end
 
+  describe "#[]" do
+    it "should access attributes" do
+      expect(described_class.new(attributes: {foo: :bar})[:foo]).to eql :bar
+    end
+  end
+
+  describe '#find_form' do
+    it 'should find a form by name' do
+      expect(resource
+              .add_form(Yaks::Resource::Form.new(name: :a_form))
+              .add_form(Yaks::Resource::Form.new(name: :b_form))
+              .find_form(:b_form))
+        .to eq Yaks::Resource::Form.new(name: :b_form)
+    end
+  end
+
+  describe "#seq" do
+    it "should provide an enumerable that yields this resource once" do
+      expect(resource.seq.each.to_a).to eql [resource]
+    end
+  end
+
   describe '#self_link' do
     let(:init_opts) {
       { links:
@@ -123,10 +70,36 @@ RSpec.describe Yaks::Resource do
     end
   end
 
+  describe "#collection?" do
+    it "should be false" do
+      expect(resource.collection?).to be false
+    end
+  end
+
+  describe "#null_resource?" do
+    it "should be false" do
+      expect(resource.null_resource?).to be false
+    end
+  end
+
   describe '#add_rel' do
     it 'should add to the rels' do
       expect(resource.add_rel(:foo).add_rel(:bar))
         .to eql Yaks::Resource.new(rels: [:foo, :bar])
+    end
+  end
+
+  describe "#add_link" do
+    let(:init_opts) {{
+      links: [Yaks::Resource::Link.new(rel: :next, uri: '/next')]
+    }}
+
+    it "should append to the links list" do
+      expect(resource.add_link(Yaks::Resource::Link.new(rel: :previous, uri: '/previous')))
+        .to eql Yaks::Resource.new(links: [
+          Yaks::Resource::Link.new(rel: :next, uri: '/next'),
+          Yaks::Resource::Link.new(rel: :previous, uri: '/previous')
+        ])
     end
   end
 
@@ -137,15 +110,21 @@ RSpec.describe Yaks::Resource do
     end
   end
 
-  describe '#find_form' do
-    it 'should find a form by name' do
-      expect(resource
-              .add_form(Yaks::Resource::Form.new(name: :a_form))
-              .add_form(Yaks::Resource::Form.new(name: :b_form))
-              .find_form(:b_form))
-        .to eq Yaks::Resource::Form.new(name: :b_form)
+  describe "#add_subresource" do
+    let(:init_opts) {{
+      subresources: [Yaks::Resource.new(attributes: {foo: 1})]
+    }}
+    it "should append to the subresources list" do
+      expect(resource.add_subresource(Yaks::Resource.new(attributes: {bar: 2})))
+        .to eql Yaks::Resource.new(
+          subresources: [
+            Yaks::Resource.new(attributes: {foo: 1}),
+            Yaks::Resource.new(attributes: {bar: 2})
+          ]
+        )
     end
   end
+
 
   describe '#members' do
     it 'should raise unsupported operation error' do
@@ -155,9 +134,17 @@ RSpec.describe Yaks::Resource do
     end
   end
 
+  describe "#merge_attributes" do
+    let(:init_opts) {{ attributes: {foo: 1, bar: 2} }}
+    it "should merge attributes into any existing attributes" do
+      expect(resource.merge_attributes(bar: 3, baz: 4))
+        .to eql Yaks::Resource.new(attributes: {foo: 1, bar: 3, baz: 4})
+    end
+  end
+
   describe '#with_collection' do
     it 'should be a no-op' do
-      expect(Yaks::Resource.new.with_collection([:foo])).to eql Yaks::Resource.new
+      expect(described_class.new.with_collection([:foo])).to eql Yaks::Resource.new
     end
   end
 
