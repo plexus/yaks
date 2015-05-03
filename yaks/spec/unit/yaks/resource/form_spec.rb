316616e683fd55b64170055cdb11d4752bf7a697
Piotr Szotkowski <chastell@chastell.net>
2015-05-03 19:22:13 +0200

    RuboCop: fix Style/IndentationConsistency

--- yaks/spec/unit/yaks/resource/form_spec.rb
+++ yaks/spec/unit/yaks/resource/form_spec.rb
@@ -58,10 +58,10 @@ RSpec.describe Yaks::Resource::Form do
       expect(form.has_action?).to be true
      end
 
-    it 'should return false if form has not an action url' do
-      form = Yaks::Resource::Form.new(name: :foo)
+     it 'should return false if form has not an action url' do
+       form = Yaks::Resource::Form.new(name: :foo)
 
-      expect(form.has_action?).to be false
-    end
+       expect(form.has_action?).to be false
+     end
   end
 end
