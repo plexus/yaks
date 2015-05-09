9d11bf2c1bf20943bad28d1e36202caf082c3137
Arne Brasseur <arne@arnebrasseur.net>
2015-05-09 16:26:28 +0200

    Refactor the acceptance specs
    I hope they are more clear and explicit this way.

--- yaks/spec/acceptance/acceptance_spec.rb
+++ yaks/spec/acceptance/acceptance_spec.rb
@@ -2,49 +2,74 @@ require 'acceptance/models'
 require 'acceptance/json_shared_examples'
 
 RSpec.describe Yaks::Format::Hal do
-  yaks_rel_template = Yaks.new do
-    format_options :hal, plural_links: ['http://literature.example.com/rels/quotes']
-    rel_template "http://literature.example.com/rel/{rel}"
+  let(:format_name) { :hal }
+
+  context 'with a configured rel template' do
+    let(:yaks_config) {
+      Yaks.new do
+        format_options :hal, plural_links: ['http://literature.example.com/rels/quotes']
+        rel_template "http://literature.example.com/rel/{rel}"
+      end
+    }
+
+    include_examples 'JSON Writer',     'confucius'
+    include_examples 'JSON round trip', 'confucius'
   end
 
-  yaks_policy_dsl = Yaks.new do
-    format_options :hal, plural_links: ['http://literature.example.com/rels/quotes']
-    derive_rel_from_association do |association|
-      "http://literature.example.com/rel/#{association.name}"
-    end
-  end
+  context 'with a rel computed by a policy override' do
+    let(:yaks_config) {
+      Yaks.new do
+        format_options :hal, plural_links: ['http://literature.example.com/rels/quotes']
+        derive_rel_from_association do |association|
+          "http://literature.example.com/rel/#{association.name}"
+        end
+      end
+    }
 
-  context  { include_examples 'JSON Writer', yaks_rel_template, :hal, 'confucius' }
-  context  { include_examples 'JSON Writer', yaks_policy_dsl, :hal, 'confucius' }
-  context  { include_examples 'JSON round trip', yaks_rel_template, :hal, 'confucius' }
-  context  { include_examples 'JSON round trip', yaks_policy_dsl, :hal, 'confucius' }
-  context  { include_examples 'JSON Writer', yaks_policy_dsl, :hal, 'list_of_quotes' }
-  context  { include_examples 'JSON round trip', yaks_policy_dsl, :hal, 'list_of_quotes' }
+    include_examples 'JSON Writer',     'confucius'
+    include_examples 'JSON round trip', 'confucius'
+    include_examples 'JSON Writer',     'list_of_quotes'
+    include_examples 'JSON round trip', 'list_of_quotes'
+  end
 end
 
 RSpec.describe Yaks::Format::Halo do
-  yaks = Yaks.new do
-    default_format :halo
-    rel_template "http://literature.example.com/rel/{rel}"
-  end
+  let(:format_name) { :halo }
+  let(:yaks_config) {
+    Yaks.new do
+      default_format :halo
+      rel_template "http://literature.example.com/rel/{rel}"
+    end
+  }
 
-  context { include_examples 'JSON Writer', yaks, :halo, 'confucius' }
+  include_examples 'JSON Writer', 'confucius'
 end
 
 RSpec.describe Yaks::Format::JsonAPI do
-  context { include_examples 'JSON Writer', Yaks.new, :json_api, 'confucius' }
-  # context { include_examples 'JSON Reader', Yaks.new, :json_api, 'confucius' }
-  context { include_examples 'JSON round trip', Yaks.new, :json_api, 'confucius' }
-  context { include_examples 'JSON Writer', Yaks.new, :json_api, 'list_of_quotes' }
-  # context { include_examples 'JSON round trip', Yaks.new, :json_api, 'list_of_quotes' }
+  let(:format_name) { :json_api }
+  let(:yaks_config) { Yaks.new }
+
+  include_examples 'JSON Writer', 'confucius'
+  # include_examples 'JSON Reader', 'confucius'
+  include_examples 'JSON round trip', 'confucius'
+  include_examples 'JSON Writer', 'list_of_quotes'
+  # include_examples 'JSON round trip', 'list_of_quotes'
 end
 
 RSpec.describe Yaks::Format::CollectionJson do
-  youtypeit_yaks = Yaks.new do
-    mapper_namespace Youtypeitwepostit
-  end
+  let(:format_name) { :collection_json }
+  let(:yaks_config) { Yaks.new }
 
-  context { include_examples 'JSON Writer', youtypeit_yaks, :collection_json, 'youtypeitwepostit' }
-  context { include_examples 'JSON Writer', Yaks.new, :collection_json, 'confucius' }
-  context { include_examples 'JSON Writer', Yaks.new, :collection_json, 'list_of_quotes' }
+  include_examples 'JSON Writer', 'confucius'
+  include_examples 'JSON Writer', 'list_of_quotes'
+
+  context 'with a namespace' do
+    let(:yaks_config) {
+      Yaks.new do
+        mapper_namespace Youtypeitwepostit
+      end
+    }
+
+    include_examples 'JSON Writer',  'youtypeitwepostit'
+  end
 end
--- yaks/spec/acceptance/json_shared_examples.rb
+++ yaks/spec/acceptance/json_shared_examples.rb
@@ -1,41 +1,52 @@
-RSpec.shared_examples_for 'JSON Writer' do |yaks, format, name|
-  let(:object)  { load_yaml_fixture(name) }
-  let(:expected) { load_json_fixture("#{name}.#{format}") }
-  let(:serialized) { yaks.call(object, hooks: [[:skip, :serialize]], format: format) }
+RSpec.shared_examples_for 'JSON Writer' do |fixture_name|
+  describe 'Yaks::Resource => JSON' do
+    let(:object)  { load_yaml_fixture(fixture_name) }
+    let(:json_fixture) { load_json_fixture("#{fixture_name}.#{format_name}") }
+    let(:serialized) {
+      yaks_config.call(object, hooks: [[:skip, :serialize]], format: format_name)
+    }
 
-  # before do
-  #   puts "============================expected=========================================="
-  #   puts JSON.pretty_generate(expected)
-  #   puts "=================yaks.call(object, format: #{format.inspect})================="
-  #   puts JSON.pretty_generate(serialized)
-  # end
+    # before do
+    #   puts "============================expected========================================="
+    #   puts JSON.pretty_generate(expected)
+    #   puts "=================yaks.call(object, format: #{format.inspect})================"
+    #   puts JSON.pretty_generate(serialized)
+    # end
 
-  it do
-    expect(serialized).to deep_eql expected
+    it 'should match the JSON fixture' do
+      expect(serialized).to deep_eql json_fixture
+    end
   end
 end
 
-RSpec.shared_examples_for 'JSON Reader' do |yaks, format, name|
-  let(:object) { load_yaml_fixture(name) }
-  let(:json) { load_json_fixture("#{name}.#{format}") }
+RSpec.shared_examples_for 'JSON Reader' do |fixture_name|
+  describe 'JSON => Yaks::Resource' do
+    let(:object) { load_yaml_fixture(fixture_name) }
+    let(:json_fixture) { load_json_fixture("#{fixture_name}.#{format_name}") }
+    let(:resource) {
+      yaks_config.read(json_fixture, hooks: [[:skip, :parse]], format: format_name)
+    }
 
-  let(:resource) { yaks.read(json, hooks: [[:skip, :parse]], format: format) }
-
-  it 'should read to a Resource' do
-    expect(resource.pp).to eql yaks.map(object).pp
+    it 'should equal the corresponding Yaks::Resource' do
+      # Comparing type+to_h to get better RSpec output upon failure
+      expect(resource).to be_a Yaks::Resource
+      expect(resource.to_h).to eql yaks.map(object).to_h
+    end
   end
 end
 
-RSpec.shared_examples_for 'JSON round trip' do |yaks, format, name|
-  let(:json_fixture) { load_json_fixture("#{name}.#{format}") }
+RSpec.shared_examples_for 'JSON round trip' do |fixture_name|
+  describe 'JSON => Yaks::Resource => JSON' do
+    let(:json_fixture) { load_json_fixture("#{fixture_name}.#{format_name}") }
+    let(:read_and_written) {
+      config = yaks_config.with(default_format: format_name)
+      config.format(
+        config.read(json_fixture, hooks: [[:skip, :parse]])
+      )
+    }
 
-  let(:read_and_written) {
-    config = yaks.with(default_format: format)
-    config.format(config.read(json_fixture, hooks: [[:skip, :parse]]))
-  }
-
-  specify 'after reading the JSON to a Resource and writing it out again, it should be identical' do
-    expect(read_and_written).to deep_eql json_fixture
+    specify 'it should be identical' do
+      expect(read_and_written).to deep_eql json_fixture
+    end
   end
-
 end
